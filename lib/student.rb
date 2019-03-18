339710fcffbcdcb0faee4e06b7f36b8da5e1777d
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-18 00:46:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,10 @@ class Student < User
   
   attr_accessor :knowledge
   
-   @knowledge = []
+
   
   def initialize(knowledge)
-    @knowledge = knowledge
-    @knowledge << self
+     @knowledge = []
   end
   
 end
